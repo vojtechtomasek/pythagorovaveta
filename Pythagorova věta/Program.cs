b31a0ada4129916aab57b19807c995b3e918b182
Tomášek Vojtěch <Tomasek.Vo.2022@skola.ssps.cz>
2022-10-11 10:50:15 +0200

    first commit

---
+++ Pythagorova věta/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Pythagorova_věta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+          
+
+            if (a > c)
+            {
+                int tmp = c;
+                c = a;
+                a = tmp;
+            }
+            if (b>c)
+            {
+                int tmp = c;
+                b = c;
+                b = tmp;
+            }
+        
+            if (a * a + b * b == c * c)
+            {
+                Console.WriteLine("Je pravoúhlý");
+            }
+
+
+        }
+    }
+}
